5312da459d461185f50d3fb72cac22ce59074ed3
Henrik Larsen <hela4200@colorado.edu>
2016-03-14 23:02:08 -0600

    Updated keywords

--- download_tweets.py
+++ download_tweets.py
@@ -48,7 +48,7 @@ class MyListener(StreamListener):
         return True
  
 twitter_stream = Stream(auth, MyListener())
-twitter_stream.filter(track=['tequila','vodka','mojito','margarita'])    
+twitter_stream.filter(track=['tequila','vodka','absinthe','Jägermeister', 'whiskey', 'whiskey', 'rum', 'beer', 'wine', 'champagne', 'gin', 'scotch', 'bourbon', 'brandy', 'cognac', 'sake', 'white wine', 'red wine'])    
 
 
 #getTimeLine()
