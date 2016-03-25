a2f995d4b689e0c9b419e2652d4f9490eb94a17c
rohit <rohitgupta0810@gmail.com>
2016-03-25 16:21:05 -0600

    adding log statemetnt

--- insert_in_mongo.py
+++ insert_in_mongo.py
@@ -4,7 +4,7 @@ import pymongo
 import sys
 import json
 
-connection = pymongo.MongoClient("mongodb://localhost")
+connection = pymongo.MongoClient("")
 db=connection.tags
 tweets = db.tweets
 
@@ -15,7 +15,8 @@ def main(jsonfile):
 	for line in f.readlines():
 		try:
 			tweet_dict = json.loads(line)
-			tweets.insert_one(tweet_dict)
+			result = tweets.insert_one(tweet_dict)
+			print result.inserted_id
 		except 	Exception as e:
 			print "Unexpected error:", type(e), e
 			sys.exit()
