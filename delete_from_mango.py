e3fa51897e29ea69540b63ad99bc2aa736e84e29
rohit <rohitgupta0810@gmail.com>
2016-03-27 12:02:24 -0600

    script to delete from mongo

---
+++ delete_from_mango.py
@@ -0,0 +1,42 @@
+''' Python script to insert tweets in mongodb
+'''
+import pymongo
+import sys
+import json
+
+connection = pymongo.MongoClient("mongodb://ec2-52-38-4-39.us-west-2.compute.amazonaws.com:27011")
+db=connection.tags
+tweets = db.tweets
+
+
+def main(jsonfile):
+	f = open(jsonfile)
+
+	for line in f.readlines():
+		try:
+			tweet_dict = json.loads(line)
+			tweet_id = tweet_dict['id']
+			remove(tweet_id)
+		except 	Exception as e:
+			print "Unexpected error:", type(e), e
+			sys.exit()
+        
+
+def remove(id):
+    '''Return all of the teams in western confence
+    '''
+    print id
+    query = {'id':long(id)}
+
+    print tweets.remove(query)
+
+
+
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print('Please provide the json file to delete data from')
+		sys.exit()
+
+	main(sys.argv[1])
+
